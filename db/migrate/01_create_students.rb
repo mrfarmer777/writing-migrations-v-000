336a6a6453bc0d09f9ad8d07abe99ec31a93d5fa
Matt Farmer <mrfarmer777@gmail.com>
2018-06-16 18:12:01 +0000

    Done.

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,2 +1,12 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
+  def create
+    create_table :students do |t|
+    end
+  end
+
+  def change
+    create_table :students do |t|
+      t.string :name
+    end
+  end
 end
