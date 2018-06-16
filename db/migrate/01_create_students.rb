d0c816b1f879e62b02ca1e2dcb15baf08ed0a48f
Matt Farmer <mrfarmer777@gmail.com>
2018-06-16 17:45:29 +0000

    automatically backed up by learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,2 +1,8 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
+  def create
+    connection=ActiveRecord::Base.establish_connection(
+    :adapter=>"sqlite3",
+    :database=>"db/students.sqlite"
+    )
+  end
 end
